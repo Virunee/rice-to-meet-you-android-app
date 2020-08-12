23e076563a34704946847a2cdf2a6a30890c4cf1
Virunee <viravongv@gmail.com>
2020-08-12 17:39:47 +0100

    get sounds to play

--- app/src/main/java/com/virunee/ricetomeetyousoundboard/AllActivity.kt
+++ app/src/main/java/com/virunee/ricetomeetyousoundboard/AllActivity.kt
@@ -12,33 +12,35 @@ import kotlinx.android.synthetic.main.activity_all.*
 
 class AllActivity : AppCompatActivity() {
 
-    private val mediaPlayer: MediaPlayer = MediaPlayer()
+    private var mediaPlayer: MediaPlayer = MediaPlayer()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_all)
 
         var sounds = ArrayList<Sound>()
-        sounds.add(Sound("ning ah", "NING AH NING AH"))
-        sounds.add(Sound("eat my p", "You are eating my pussy, boy"))
-        sounds.add(Sound("careless whisper","careless whisper"))
-        sounds.add(Sound("you make me feel special","you make me feel special"))
-        sounds.add(Sound("airhorn","airhorn"))
-        sounds.add(Sound("finish him","finish him"))
-        sounds.add(Sound("marvin gaye","marvin gaye"))
-        sounds.add(Sound("mongolia","mongolia"))
-        sounds.add(Sound("lonely af","lonely af"))
-        sounds.add(Sound("R2MU intro","R2MU intro"))
-        sounds.add(Sound("R2MU sting","R2MU sting"))
-        sounds.add(Sound("R2MU outro","R2MU outro"))
+        sounds.add(Sound("ning ah", "NING AH NING AH", R.raw.ning_ah))
+        sounds.add(Sound("eat my p", "You are eating my pussy, boy", R.raw.eat_my_p))
+        sounds.add(Sound("careless whisper","careless whisper", R.raw.careless_whisper))
+        sounds.add(Sound("you make me feel special","you make me feel special", R.raw.special))
+        sounds.add(Sound("airhorn","airhorn", R.raw.airhorn))
+        sounds.add(Sound("finish him","finish him", R.raw.finish_him))
+        sounds.add(Sound("marvin gaye","marvin gaye", R.raw.marvin_gaye))
+        sounds.add(Sound("mongolia","mongolia", R.raw.mongolia))
+        sounds.add(Sound("lonely af","lonely af", R.raw.lonely_af))
+        sounds.add(Sound("R2MU intro","R2MU intro", R.raw.r2mu_intro))
+        sounds.add(Sound("R2MU sting","R2MU sting", R.raw.r2mu_sting))
+        sounds.add(Sound("R2MU outro","R2MU outro", R.raw.r2mu_outro))
 
         val soundsAdapter = SoundAdapter(this, sounds, R.color.colorYellow)
         val soundView: ListView = list_view_all
         soundView.adapter = soundsAdapter
 
         soundView.setOnItemClickListener{ parent, view, position, id ->
-            Toast.makeText(this, "Clicked item :"+" "+position,Toast.LENGTH_SHORT).show()
+            mediaPlayer = MediaPlayer.create(this, sounds.get(position).getAudioResource())
+            mediaPlayer.start()
         }
+
 
     }
 }
--- app/src/main/java/com/virunee/ricetomeetyousoundboard/Sound.kt
+++ app/src/main/java/com/virunee/ricetomeetyousoundboard/Sound.kt
@@ -4,13 +4,15 @@ package com.virunee.ricetomeetyousoundboard;
  * {@link Sound} represents a sound bite
  * It contains the title of the sound bite and a description
  */
-public class Sound(title: String, description: String) {
+public class Sound(title: String, description: String, audioResourceId: Int) {
 
     /** title for the sound */
     private var mTitle: String = title
 
     /** description of the sound */
     private var mDescription: String = description
+
+    private var mAudioFile: Int = audioResourceId
 
 //    fun Sound(title: String, description: String) {
 //        mTitle = title
@@ -23,6 +25,10 @@ public class Sound(title: String, description: String) {
 
     fun getDescription() : String {
         return mDescription
+    }
+
+    fun getAudioResource(): Int {
+        return mAudioFile
     }
 
 
