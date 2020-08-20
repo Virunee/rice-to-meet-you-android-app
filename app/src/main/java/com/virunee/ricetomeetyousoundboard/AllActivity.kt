5fc210eabf368a09e17bbd511e67b37c025b8fe5
Virunee <viravongv@gmail.com>
2020-08-20 23:23:09 +0100

    manage audio focus
    and added some more sounds

--- app/src/main/java/com/virunee/ricetomeetyousoundboard/AllActivity.kt
+++ app/src/main/java/com/virunee/ricetomeetyousoundboard/AllActivity.kt
@@ -1,9 +1,15 @@
 package com.virunee.ricetomeetyousoundboard
 
+import android.content.Context
+import android.media.AudioAttributes
+import android.media.AudioFocusRequest
+import android.media.AudioManager
 import android.media.MediaPlayer
 import android.media.MediaPlayer.OnCompletionListener
+import android.os.Build
 import android.os.Bundle
 import android.widget.ListView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_all.*
 
@@ -11,7 +17,9 @@ import kotlinx.android.synthetic.main.activity_all.*
 class AllActivity : AppCompatActivity() {
 
     private var mediaPlayer: MediaPlayer? = null
+    private var audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_all)
@@ -29,22 +37,55 @@ class AllActivity : AppCompatActivity() {
         sounds.add(Sound("R2MU intro","R2MU intro", R.raw.r2mu_intro))
         sounds.add(Sound("R2MU sting","R2MU sting", R.raw.r2mu_sting))
         sounds.add(Sound("R2MU outro","R2MU outro", R.raw.r2mu_outro))
+        sounds.add(Sound("Good comedians","How good of a comedian are we", R.raw.evelyn_good_comedian))
+        sounds.add(Sound("Nigellll!","Nigellll!", R.raw.evelyn_nigel))
+        sounds.add(Sound("No noooo","No noooo!", R.raw.nigel_no_noooo))
+        sounds.add(Sound("Stop improving","Stop improving yourself", R.raw.nigel_stop_improving_yourself))
+        sounds.add(Sound("You're such a dick","Nigelll, you're such a dick!", R.raw.evelyn_ysad))
+        sounds.add(Sound("Why like this","Haiyaa, why you like this?", R.raw.nigel_why_like_this))
 
         val soundsAdapter = SoundAdapter(this, sounds, R.color.colorYellow)
         val soundView: ListView = list_view_all
         soundView.adapter = soundsAdapter
 
+        // Request audio focus so in order to play the audio file. The app needs to play a
+        // short audio file, so we will request audio focus with a short amount of time
+        // with AUDIOFOCUS_GAIN_TRANSIENT.
+        val focusRequest = AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN).run{
+            setAudioAttributes(AudioAttributes.Builder().run {
+                setUsage(AudioAttributes.USAGE_MEDIA)
+                setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                build()
+            })
+            setOnAudioFocusChangeListener(onAudioFocusChangeListener)
+            build()
+        }
+
         soundView.setOnItemClickListener{ parent, view, position, id ->
             releaseMediaPlayer()
-            mediaPlayer = MediaPlayer.create(this, sounds.get(position).getAudioResource())
-            mediaPlayer?.start()
-            mediaPlayer?.setOnCompletionListener(completionListener)
+
+            // Request audio focus so in order to play the audio file. The app needs to play a
+            // short audio file, so we will request audio focus with a short amount of time
+            // with AUDIOFOCUS_GAIN_TRANSIENT.
+            val result = audioManager.requestAudioFocus(focusRequest)
+            if(result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                // We have audio focus now
+
+                // Create and setup the Media Player for the audio resource associate with the current word
+                mediaPlayer = MediaPlayer.create(this, sounds[position].getAudioResource())
+
+                // Start the audio file
+                mediaPlayer?.start()
+
+                // Set up the onCompletionListener on the media player so we can stop and release the player once the audio has finished playing.
+                mediaPlayer?.setOnCompletionListener(completionListener)
+            }
         }
     }
 
     override fun onStop() {
-        releaseMediaPlayer()
         super.onStop()
+        releaseMediaPlayer()
     }
 
     /**
@@ -55,13 +96,43 @@ class AllActivity : AppCompatActivity() {
             releaseMediaPlayer()
         }
 
-    fun releaseMediaPlayer() {
+    /**
+     * This listener gets triggered whenever the audio focus changes
+     * (i.e., we gain or lose audio focus because of another app or device).
+     */
+    private var onAudioFocusChangeListener: AudioManager.OnAudioFocusChangeListener = AudioManager.OnAudioFocusChangeListener {focusChange ->
+        if(focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT || focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+            // The AUDIOFOCUS_LOSS_TRANSIENT case means that we've lost audio focus for a
+            // short amount of time. The AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK case means that
+            // our app is allowed to continue playing sound but at a lower volume. We'll treat
+            // both cases the same way because our app is playing short sound files.
+
+            // Pause playback and reset player to the start of the file. That way, we can
+            // play the word from the beginning when we resume playback.
+            mediaPlayer?.pause();
+//            mediaPlayer?.seekTo(0);
+        } else if(focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+            // We've regained focus and can resume playing.
+            mediaPlayer?.start()
+        } else if(focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+            // The AUDIOFOCUS_LOSS case means we've lost audio focus and
+            // Stop playback and clean up resources
+            mediaPlayer?.stop()
+            releaseMediaPlayer()
+        }
+    }
+
+    private fun releaseMediaPlayer() {
         // If the media player is not null, then it may be currently playing a sound.
         if (mediaPlayer != null) {
             // Regardless of the current state of the media player, release its resources
             // because we no longer need it.
             mediaPlayer?.release();
             mediaPlayer = null
+
+            // Regardless of whether or not we were granted audio focus, abandon it. This also
+            // unregisters the AudioFocusChangeListener so we don't get anymore callbacks.
+            audioManager.abandonAudioFocus(onAudioFocusChangeListener)
         }
     }
 }
